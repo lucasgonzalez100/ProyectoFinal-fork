db9b74280fd2918fd97cffbb8b4e5100eef6e701
Lucas Gonzalez <gonzalezlucasnicolas69@gmail.com>
2024-05-23 17:55:34 -0300

    Mensaje del commit

--- app.js
+++ app.js
@@ -8,23 +8,11 @@ const  mongoose  = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config();
-// const session = require ('express-session');
-// const FileStore = require ('session-file-store');
-
-// const fileStore= FileStore(session)
-// app.use(session({
-//   secret:'CoderHouse',
-//   store: MongoStore.create({
-//     mongoUrl:'mongodb+srv://gonzalezlucasnicolas69:123@cluster0.xi8psab.mongodb.net/?retryWrites=true&w=majority'
-//   }),
-//   resave: true,
-//   saveUninitialized: true
-// }));
+// dale que sos vos lucas
 
 const server = httpServer.listen(8080, () => {
   console.log("Servidor escuchando en el puerto 8080");
 });
-
 const bodyParser = require('body-parser'); 
 
 app.use(express.json());
